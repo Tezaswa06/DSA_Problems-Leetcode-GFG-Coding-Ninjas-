8f12fce3978780c020f333a9d7920e9dabb8e25b
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-02-22 19:37:05 +0530

    Time: 3 ms (99.16%), Space: 16 MB (40.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -29,26 +29,67 @@ public:
     //     }
 
     // }
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<bool>Rows(matrix.size());
-        vector<bool>Cols(matrix[0].size());
 
-        // Queries to be run for Rows and Cols array to set true if we found 0 in the matrix
+    // Approach 2
+
+    // void setZeroes(vector<vector<int>>& matrix) {
+    //     vector<bool>Rows(matrix.size());
+    //     vector<bool>Cols(matrix[0].size());
+
+    //     // Queries to be run for Rows and Cols array to set true if we found 0 in the matrix
+
+    //     for(int row=0;row<matrix.size();row++){
+    //         for(int col=0;col<matrix[0].size();col++){
+    //             if(matrix[row][col]==0){
+    //                 Rows[row] = Cols[col] = true;
+    //             }
+    //         }
+    //     }
+    //     // updating each and every matrix element as we found that particular row or col to be true
+    //             for(int row=0;row<matrix.size();row++){
+    //                 for(int col=0;col<matrix[0].size();col++){
+    //                     if(Rows[row]==true || Cols[col]==true){
+    //                         matrix[row][col] = 0;
+    //                     }
+    //                 }
+    //            }
+    // }
+
+    // Approach 3
+
+    void setZeroes(vector<vector<int>>& matrix){
+        bool zerothrow = false;
+        bool zerothcol = false;
 
         for(int row=0;row<matrix.size();row++){
             for(int col=0;col<matrix[0].size();col++){
                 if(matrix[row][col]==0){
-                    Rows[row] = Cols[col] = true;
+                    matrix[0][col] = matrix[row][0] = 0;
+                if(row == 0){
+                    zerothrow = true;
+                }
+                if(col == 0){
+                    zerothcol = true;
+                }
                 }
             }
         }
-        // updating each and every matrix element as we found that particular row or col to be true
-                for(int row=0;row<matrix.size();row++){
-                    for(int col=0;col<matrix[0].size();col++){
-                        if(Rows[row]==true || Cols[col]==true){
-                            matrix[row][col] = 0;
-                        }
-                    }
+        for(int row=1;row<matrix.size();row++){
+            for(int col=1;col<matrix[0].size();col++){
+                if(matrix[0][col]==0 || matrix[row][0]==0){
+                    matrix[row][col] = 0;
                 }
+            }
+        }
+        if(zerothrow == true){
+            for(int col=0;col<matrix[0].size();col++){
+                matrix[0][col] = 0;
+            }
+        }
+        if(zerothcol == true){
+            for(int row=0;row<matrix.size();row++){
+                matrix[row][0] = 0;
+            }
+        }
     }
 };
