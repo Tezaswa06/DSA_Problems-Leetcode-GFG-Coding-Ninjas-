85d1bd58a42a6c7d7541d03369c6916ce3d2c5a3
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-03-18 17:27:58 +0530

    Time: 123 ms (38.91%), Space: 81.7 MB (42.98%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>>noOfways;
 
-    void printPaths(int n,int k, vector<int>& output){
-        if(k == 0){ // positive base case when there is nothing left for selection
-            noOfways.push_back(output);
+    // Approach 1...........
+
+    // vector<vector<int>>noOfways;
+
+    // void printPaths(int n,int k, vector<int>& output){
+    //     if(k == 0){ // positive base case when there is nothing left for selection
+    //         noOfways.push_back(output);
+    //         return ;
+    //     }
+    //     if(n == 0 || k > n){ // when no of selection is grater than no of elements and when there is no item to select 
+    //         return;
+    //     }
+
+    //     output.push_back(n);
+    //     printPaths(n - 1,k - 1,output); // yes choice when we select the item
+    //     output.pop_back();
+
+    //     printPaths(n - 1,k,output); //  no choice here k will remain same as we have not selected anything
+
+    // }
+    // vector<vector<int>> combine(int n, int k) {
+    //     vector<int>output; // which will store all the elements in the current path
+    //     printPaths(n , k , output);
+    //     return noOfways;
+
+    // }
+    
+    // Approach 2............
+    
+    vector<vector<int>> ways;
+    void printpaths(int n,int k, vector<int>& output,int lastitem){
+        if(k == 0){
+            ways.push_back(output);
             return ;
         }
-        if(n == 0 || k > n){ // when no of selection is grater than no of elements and when there is no item to select 
-            return;
+
+        for(int item = lastitem + 1 ; item <= n ; item ++){ // loop for choices which item to choose
+            output.push_back(item);
+            printpaths(n , k - 1,output , item);
+            output.pop_back();
+
         }
-
-        output.push_back(n);
-        printPaths(n - 1,k - 1,output); // yes choice when we select the item
-        output.pop_back();
-
-        printPaths(n - 1,k,output); //  no choice here k will remain same as we have not selected anything
-
     }
-    vector<vector<int>> combine(int n, int k) {
-        vector<int>output; // which will store all the elements in the current path
-        printPaths(n , k , output);
-        return noOfways;
-
+    vector<vector<int>> combine(int n, int k){
+        vector<int>output;
+        printpaths(n,k,output,0);
+        return ways;
     }
+    
 };
