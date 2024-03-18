cbed1bff63d837ad7f6f1dd9d69a9e46905311ba
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-03-18 18:47:31 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (31.93%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -8,13 +8,13 @@ public:
             return ;
         }
         for(int index = 0 ; index < nums.size() ; index++){
-            if(visited[index] == true){
+            if(visited[index] == true){ // if the current index is true then then that index will bw skipped
                 continue;
             }
             output.push_back(nums[index]);
             visited[index] = true;
 
-            printPaths(nums,output,visited);
+            printPaths(nums,output,visited); // here we are only rearranging the items so we want all the items that is why there is no no choice during recursion call
 
             output.pop_back();
             visited[index] = false;
