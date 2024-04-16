c5532623f7f00e3125dd980125824358631ecc09
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-04-16 09:07:31 +0530

    Time: 15 ms (70.41%), Space: 22.1 MB (8.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,6 +10,44 @@
  */
 class Solution {
 public:
+    // approach 1 merging two linked lists
+    // ListNode *Merge(ListNode *list1 ,ListNode *list2){
+    //     ListNode *ptr1 = list1;
+    //     ListNode *ptr2 = list2;
+    //     ListNode *dummy = new ListNode(-1);
+    //     ListNode *tail = dummy;
+
+    //     while(ptr1 != nullptr && ptr2 != nullptr){
+    //         if(ptr1 -> val < ptr2 -> val){
+    //             tail -> next = ptr1;
+    //             tail = ptr1;
+    //             ptr1 = ptr1 -> next;
+    //         }
+    //         else{
+    //             tail -> next = ptr2;
+    //             tail = ptr2;
+    //             ptr2 = ptr2 -> next;
+    //         }
+    //     }
+    //     if(ptr1 != nullptr){
+    //         tail -> next = ptr1;
+    //     }
+    //     else{
+    //         tail -> next = ptr2;
+    //     }
+    //     return dummy -> next;
+    // }
+    // ListNode* mergeKLists(vector<ListNode*>& lists) {
+    //     ListNode *res = nullptr;
+
+    //     for(ListNode *head : lists){
+    //         res = Merge(res,head);
+    //     }
+    //     return res;
+    // }
+
+
+    // Approach 2  divide and conquer 
     ListNode *Merge(ListNode *list1 ,ListNode *list2){
         ListNode *ptr1 = list1;
         ListNode *ptr2 = list2;
@@ -36,12 +74,19 @@ public:
         }
         return dummy -> next;
     }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *res = nullptr;
-
-        for(ListNode *head : lists){
-            res = Merge(res,head);
+    ListNode *Helper(vector<ListNode *>& lists,int left , int right){
+        if(left == right){
+            return lists[left];
         }
-        return res;
+        int mid = (left + right)/2;
+        ListNode *l1 = Helper(lists , left , mid);
+        ListNode *l2 = Helper(lists , mid + 1 ,right);
+        return Merge(l1 , l2);
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists){
+        if(lists.size() == 0){
+            return nullptr;
+        }
+        return Helper(lists , 0 , lists.size()-1);
     }
 };
