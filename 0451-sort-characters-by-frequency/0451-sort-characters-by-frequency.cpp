b80d9bd468ffa205cbe5c7d6e05e2d6b4d1f9a7d
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-06-07 12:39:04 +0530

    Time: 11 ms (53.11%), Space: 10.7 MB (70.14%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<int>freqarr(256);
+        unordered_map<char, int> freq;
 
-        for(char val : s){
-            freqarr[val]++;
+        // Calculate the frequency of each character
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            freq[ch]++;
         }
 
-        vector<vector<char>>buckets(s.size()+1);
-
-        for(int i = 0 ; i < 256 ; i++){
-            int count = freqarr[i];
-            if(count == 0){
-                continue;
-            }
-            buckets[count].push_back(static_cast<char>(i));
+        unordered_map<int, vector<char>> freq2;
+        for (auto it = freq.begin(); it != freq.end(); ++it) {
+            char ch = it->first;
+            int f = it->second;
+            freq2[f].push_back(ch);
         }
 
-        int index = 0;
-        for(int bucket = 1 ; bucket < buckets.size() ; bucket++){
-            for(char val : buckets[bucket]){
-                for(int freq = 0; freq < bucket ; freq++){
-                    s[index] = val;
-                    index++;
+        // Build the result string
+        string res;
+        for (int f = s.length(); f >= 1; f--) {
+            if (freq2.find(f) != freq2.end()) {
+                for (char ch : freq2[f]) {
+                    res.append(f,ch);
                 }
             }
         }
-        reverse(s.begin(),s.end());
-        return s;
+
+        return res;
     }
 };
