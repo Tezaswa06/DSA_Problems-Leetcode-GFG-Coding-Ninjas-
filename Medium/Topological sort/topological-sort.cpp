6b62302242c682b0335905827e46cab888495f84
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-06-18 12:04:11 +0530

    Update topological-sort.cpp

--- Medium/Topological sort/topological-sort.cpp
+++ Medium/Topological sort/topological-sort.cpp
@@ -4,37 +4,77 @@ using namespace std;
 
 // } Driver Code Ends
 
+// DFS
+
+// class Solution
+// {
+// 	public:
+// 	vector<int>topo;
+// 	int index;
+// 	void dfs(int src, vector<int>adj[],vector<bool>&vis){
+// 	    if(vis[src] == true){
+// 	        return;
+// 	    }
+// 	    vis[src] = true;
+// 	    for(int nbr : adj[src]){
+// 	        dfs(nbr,adj,vis);
+// 	    }
+// 	    topo[index--] = src;
+	    
+// 	}
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    vector<bool>vis(V,false);
+// 	    index = V - 1;
+// 	    topo.resize(V);
+	    
+// 	    for(int i=0; i<V; i++){
+// 	        dfs(i,adj,vis);
+// 	    }
+	    
+	    
+// 	    return topo;
+// 	}
+// };
+
+// BFS using Kahn's Algo
+
 class Solution
 {
 	public:
-	vector<int>topo;
-	int index;
-	void dfs(int src, vector<int>adj[],vector<bool>&vis){
-	    if(vis[src] == true){
-	        return;
-	    }
-	    vis[src] = true;
-	    for(int nbr : adj[src]){
-	        dfs(nbr,adj,vis);
-	    }
-	    topo[index--] = src;
-	    
-	}
+	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    vector<bool>vis(V,false);
-	    index = V - 1;
-	    topo.resize(V);
-	    
+	    vector<int> incoming(V, 0);
+        
+        // Calculate the incoming degree for each node
+        for (int i = 0; i < V; i++) {
+            for (int node : adj[i]) {
+                incoming[node]++;
+            }
+        }
+	    // multisource bfs
+	    vector<int>topo(V);
+	    int push = 0;
+	    int pop = 0;
 	    for(int i=0; i<V; i++){
-	        dfs(i,adj,vis);
+	        if(incoming[i] == 0){
+	            topo[push++] = i;
+	        }
 	    }
 	    
-	    
+	    while(push < V){
+	        int src = topo[pop++];
+	        for(int nbr : adj[src]){
+	            incoming[nbr]--;
+	            if(incoming[nbr] == 0){
+	                topo[push++] = nbr;
+	            }
+	        }
+	    }
 	    return topo;
-	}
+	 }
 };
-
 
 //{ Driver Code Starts.
 
