cb2868ff52c5f92074e9928866abe918c41621b6
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-06-18 12:35:26 +0530

    Update directed-graph-cycle.cpp

--- Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,65 +1,76 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+// DFS
 
-// } Driver Code Ends
+// class Solution {
+//   public:
+//     bool dfs(int src, vector<int> adj[] , vector<int>& vis){
+//         if(vis[src] == 1){
+//             return true;
+//         }
+//         if(vis[src] == 2){
+//             return false;
+//         }
+        
+//         vis[src] = 1;//visiting the node for the fist time in preorder
+//         for(int nbr : adj[src]){
+//             if(dfs(nbr,adj,vis) == true){
+//                 return true;
+//             }
+//         }
+//         vis[src] = 2; //  visiting node in postorder
+//         return false;
+//     }
+//     bool isCyclic(int V, vector<int> adj[]) {
+//         vector<int>vis(V);// 0 -> unvisited, 1 -> preorder , 2 -> postorder
+        
+//         for(int i=0; i<V; i++){
+//             if(vis[i] > 0){//already visited component then simply return
+//                 continue;
+//             }
+//             if(dfs(i,adj,vis) == true){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// BFS
+
 class Solution {
   public:
-    bool dfs(int src, vector<int> adj[] , vector<int>& vis){
-        if(vis[src] == 1){
-            return true;
-        }
-        if(vis[src] == 2){
-            return false;
-        }
-        
-        vis[src] = 1;//visiting the node for the fist time in preorder
-        for(int nbr : adj[src]){
-            if(dfs(nbr,adj,vis) == true){
-                return true;
+    
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int>incoming(V,0);
+        for(int i=0; i<V; i++){
+            for(int node : adj[i]){
+                incoming[node]++;
             }
         }
-        vis[src] = 2; //  visiting node in postorder
-        return false;
-    }
-    bool isCyclic(int V, vector<int> adj[]) {
-        vector<int>vis(V);// 0 -> unvisited, 1 -> preorder , 2 -> postorder
+        
+        vector<int>topo(V);
+        int push = 0;
+        int pop = 0;
         
         for(int i=0; i<V; i++){
-            if(vis[i] > 0){//already visited component then simply return
-                continue;
-            }
-            if(dfs(i,adj,vis) == true){
-                return true;
+            if(incoming[i] == 0){
+                topo[push++] = i;
             }
         }
-        return false;
+        
+        while(pop < push && push < V){
+            int src = incoming[pop++];
+            for(int nbr : adj[src]){
+                incoming[nbr]--;
+                if(incoming[nbr] == 0){
+                    topo[push++] = nbr;
+                }
+            }
+        }
+        if(push == V){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<int> adj[V];
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-        }
-
-        Solution obj;
-        cout << obj.isCyclic(V, adj) << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
