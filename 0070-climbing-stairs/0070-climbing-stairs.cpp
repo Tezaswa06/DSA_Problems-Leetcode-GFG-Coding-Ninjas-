1a9d5dd89ab7fd468e04549cf9536272c0366b86
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-07-08 10:35:28 +0530

    Time: 2 ms (49.50%), Space: 7 MB (86.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -46,8 +46,19 @@ public:
         } 
         return dp[n];
     }
+    int spaceOpt(int n){
+        int prev1 = 1;
+        int prev2 = 0;
+        for(int i=1; i<=n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
     int climbStairs(int n) {
-        vector<int>dp(n + 1);
-        return tabulation(n,dp);
+        // vector<int>dp(n + 1);
+        //return tabulation(n,dp);
+        return spaceOpt(n);
     }
 };
