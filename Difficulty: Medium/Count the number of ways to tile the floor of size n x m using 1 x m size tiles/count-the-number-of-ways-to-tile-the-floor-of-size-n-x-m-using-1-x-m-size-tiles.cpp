818cc6a22c5b3587925a108448d57c6598397232
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-07-14 10:30:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of ways to tile the floor of size n x m using 1 x m size tiles/count-the-number-of-ways-to-tile-the-floor-of-size-n-x-m-using-1-x-m-size-tiles.cpp
+++ Difficulty: Medium/Count the number of ways to tile the floor of size n x m using 1 x m size tiles/count-the-number-of-ways-to-tile-the-floor-of-size-n-x-m-using-1-x-m-size-tiles.cpp
@@ -5,31 +5,62 @@ using namespace std;
 
 // } Driver Code Ends
 
+// memoization
+
+// class Solution{
+// 	public:
+	
+// 	int mod = 1000000007;
+// 	int memo(int n,int m,vector<int>& dp){
+// 	    if(n < m){
+// 	        return 1;
+// 	    }
+// 	    if(n == m){
+// 	        return 2;
+// 	    }
+// 	    if(dp[n] != -1){
+// 	        return dp[n];
+// 	    }
+// 	    int ans1 = memo(n-1,m,dp);
+// 	    int ans2 = memo(n-m,m,dp);
+	    
+// 	    return dp[n] = (ans1 + ans2) % mod;
+// 	}
+// 	int countWays(int n, int m)
+// 	{
+// 	     vector<int>dp(n + 1,-1);
+// 	     return memo(n,m,dp);
+// 	}
+// };
+
+// space optimization
 class Solution{
 	public:
-	
-	int mod = 1000000007;
-	int memo(int n,int m,vector<int>& dp){
+	    int mod = 1000000007;
+	    
+		int countWays(int n, int m)
+	{
 	    if(n < m){
 	        return 1;
 	    }
 	    if(n == m){
 	        return 2;
 	    }
-	    if(dp[n] != -1){
-	        return dp[n];
+	    deque<int>dp;
+	    for(int i=1; i<m; i++){
+	        dp.push_back(1);// inserting the base case in the dp n < m
 	    }
-	    int ans1 = memo(n-1,m,dp);
-	    int ans2 = memo(n-m,m,dp);
+	    dp.push_back(2); //  n == m
 	    
-	    return dp[n] = (ans1 + ans2) % mod;
-	}
-	int countWays(int n, int m)
-	{
-	     vector<int>dp(n + 1,-1);
-	     return memo(n,m,dp);
+	    for(int i=m+1; i<=n; i++){
+	        int ans = (dp.front() + dp.back()) % mod; 
+	        dp.pop_front();
+	        dp.push_back(ans);
+	    }
+	    return dp.back();
 	}
 };
+
 
 //{ Driver Code Starts.
 int main(){
