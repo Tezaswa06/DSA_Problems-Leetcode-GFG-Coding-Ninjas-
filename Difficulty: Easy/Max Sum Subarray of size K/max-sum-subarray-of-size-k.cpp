3c6286d83aa38c5f5b448e7ff4f5ee35f7fb8a27
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-07-25 16:49:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
@@ -3,6 +3,7 @@
 using namespace std; 
 
 // } Driver Code Ends
+
 class Solution{   
 public:
     long maximumSumSubarray(int wind, vector<int> &arr , int N){
@@ -11,17 +12,17 @@ public:
             currSum += arr[i];
         }
         long maxSum = currSum;
-        
         for(long i=wind; i<N; i++){
-            long include = arr[i];
-            long exclude = arr[i-wind];
+            long include = arr[i];// including the current element after window size
+            long exclude = arr[i-wind];// excluding the first element to maintain the window size
             currSum += include - exclude;
             
-            maxSum = max(maxSum,currSum);
+            maxSum = max(currSum,maxSum);
         }
         return maxSum;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() 
