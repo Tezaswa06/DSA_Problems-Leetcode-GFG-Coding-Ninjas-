df8b8a376c4af514d521ccce53ba6a049a301e42
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-07-27 23:16:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest K unique characters substring/longest-k-unique-characters-substring.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        int maxLen = -1;
+        unordered_map<char,int>freq;
+        int left = 0;
+        
+        for(int right=0; right<s.size(); right++){
+            char ch = s[right];
+            freq[ch]++;
+            
+            while(freq.size() > k){
+                char leftChar = s[left];
+                freq[leftChar]--;
+                
+                if(freq[leftChar] == 0){
+                    freq.erase(leftChar);
+                }
+                left++;
+            }
+            if(freq.size() == k){
+                maxLen = max(maxLen,right-left+1);
+            }
+        }
+        return maxLen;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int k;
+        cin >> k;
+        Solution ob;
+        cout << ob.longestKSubstr(s, k) << endl;
+    }
+}
+
+// } Driver Code Ends
