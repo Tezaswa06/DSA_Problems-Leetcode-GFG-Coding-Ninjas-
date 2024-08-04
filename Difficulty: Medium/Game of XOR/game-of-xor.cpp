3bc99ba57988e3bcc795d911db8ba4beb8d3727c
Tezaswa06 <156665940+Tezaswa06@users.noreply.github.com>
2024-08-04 23:24:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Game of XOR/game-of-xor.cpp
+++ Difficulty: Medium/Game of XOR/game-of-xor.cpp
@@ -3,15 +3,31 @@
 using namespace std;
 
 // } Driver Code Ends
+
+// class Solution {
+//   public:
+//     int gameOfXor(int N , int A[]) {
+//         int res = 0;
+//         for(int i=0; i<N; i++){
+//             int occurences = (i + 1) * (N - i);
+//             if((occurences & 1) == 1){//if occurences are odd no of time then only we will be performing xor operation
+//                 res = res ^ A[i];
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution {
   public:
     int gameOfXor(int N , int A[]) {
+        if(N % 2 == 0){
+            return 0;
+        }
         int res = 0;
-        for(int i=0; i<N; i++){
-            int occurences = (i + 1) * (N - i);
-            if((occurences & 1) == 1){// if occurences are odd no of time then only we will be performing xor operation
-                res = res ^ A[i];
-            }
+        for(int i=0; i<N; i+=2){
+            res = res ^ A[i];
         }
         return res;
     }
